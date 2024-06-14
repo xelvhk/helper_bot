9360b22eef0f097084f4d239f96891f474d70743
Lex <khlex93@gmail.com>
2024-06-15 01:04:04 +0300

    Services check

--- services/services.py
+++ services/services.py
@@ -3,28 +3,6 @@ from lexicon.lexicon_ru import LEXICON_RU
 from keyboards.keyboards import tasks_kb, weather_kb, happy_kb
 import requests
 
-
-# Функция, возвращающая ключ из словаря, по которому
-# хранится значение, передаваемое как аргумент - выбор пользователя
-def normalize_user_answer(user_answer: str) -> str:
-    for key in LEXICON_RU:
-        if LEXICON_RU[key] == user_answer:
-            break
-    return key
-
-
-def select_point(user_answer: str) -> str:
-    for key in LEXICON_RU:
-        if user_answer == 'weather':
-            return weather_kb
-        elif user_answer == 'tasks':
-            return tasks_kb
-        elif user_answer == 'happy':
-            return happy_kb
-        else:
-            return LEXICON_RU['other_answer']
-
-
 # Прогноз погоды по API
 def get_weather(city: str) -> str:
     # Координаты города: Saint Petersburg, Russia
